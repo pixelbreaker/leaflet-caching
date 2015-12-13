2e44a64aa5227bfb6dd96e2a42232cea13815eda
Gabriel Mak <pixelbreaker@gmail.com>
2015-12-13 14:02:37 +0000

    fixing cors proxying

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var basicAuth = require('basic-auth');
 var app = express();
+var jade = require('jade');
+var request = require('request');
 
 var jetpack = require('fs-jetpack');
 var dotenv = require('dotenv');
@@ -18,7 +20,7 @@ console.log(process.env.USER);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(function (req, res, next) {
+auth = function (req, res, next) {
   function unauthorized(res) {
     res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
     return res.sendStatus(401);
@@ -35,13 +37,38 @@ app.use(function (req, res, next) {
   } else {
     return unauthorized(res);
   };
-});
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.all(/^\/cors\/(.*)$/, function (req, res, next) {
+  // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+  if (req.method === 'OPTIONS') {
+    // CORS Preflight
+    res.send();
+  } else {
+    var targetURL = 'http://' + req.params[0];//req.header('Target-URL');
+    if (!targetURL) {
+      res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+      return;
+    }
+    request({ url: targetURL, method: req.method, json: req.body, qs:req.query, headers: {'Authorization': req.header('Authorization')} },
+      function (error, response, body) {
+        if (error) {
+          console.error('error: ' + response.statusCode)
+        }
+      }).pipe(res);
+  }
+});
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(auth, express.static(path.join(__dirname, 'client')));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,24 +80,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+  // app.use(function(err, req, res, next) {
+  //   res.status(err.status || 500);
+  //   res.render('error', {
+  //     message: err.message,
+  //     error: err
+  //   });
+  // });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 listen();
 
